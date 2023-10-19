7f9da8bf9a7d9db95fa079e499d42a7804b68e3c
Guillaume FROIDCOURT <guillaume.froidcourt@etu.u-bordeaux.fr>
2023-10-19 10:24:41 +0200

    rollback

--- src/grid.c
+++ src/grid.c
@@ -10,63 +10,69 @@
 #include <stdlib.h>
 
 /* Internat structure (hidden from outside) for a sudoku grid*/
-struct _grid_t {
+struct _grid_t
+{
   size_t size;
-  colors_t** cells;
+  colors_t **cells;
 };
 
-bool
-grid_check_char(const grid_t* grid, const char c) {
-  if (!grid) {
+bool grid_check_char(const grid_t *grid, const char c)
+{
+  if (!grid)
+  {
     return false;
   }
 
-  switch (grid->size) {
-    case 64:
-      return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'Z') || c == '@'
-             || (c >= 'a' && c <= 'z') || c == '&' || c == '*' || c == '_';
-    case 49:
-      return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'Z') || c == '@'
-             || (c >= 'a' && c <= 'm') || c == '_';
-    case 36:
-      return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'Z') || c == '@'
-             || c == '_';
-    case 25:
-      return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'P') || c == '_';
-    case 16:
-      return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'G') || c == '_';
-    case 9:
-      return (c >= '1' && c <= '9') || c == '_';
-    case 4:
-      return (c >= '1' && c <= '4') || c == '_';
-    case 1:
-      return c == '1' || c == '_';
-    default:
-      return false;
+  switch (grid->size)
+  {
+  case 64:
+    return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'Z') || c == '@' || (c >= 'a' && c <= 'z') || c == '&' || c == '*' || c == '_';
+  case 49:
+    return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'Z') || c == '@' || (c >= 'a' && c <= 'm') || c == '_';
+  case 36:
+    return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'Z') || c == '@' || c == '_';
+  case 25:
+    return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'P') || c == '_';
+  case 16:
+    return (c >= '1' && c <= '9') || (c >= 'A' && c <= 'G') || c == '_';
+  case 9:
+    return (c >= '1' && c <= '9') || c == '_';
+  case 4:
+    return (c >= '1' && c <= '4') || c == '_';
+  case 1:
+    return c == '1' || c == '_';
+  default:
+    return false;
   }
 }
 
-grid_t*
-grid_alloc(size_t size) {
-  if (size == 0 || !grid_check_size(size)) {
+grid_t *
+grid_alloc(size_t size)
+{
+  if (size == 0 || !grid_check_size(size))
+  {
     fprintf(stderr, "Invalid grid size: %zu\n", size);
     return NULL;
   }
 
-  grid_t* grid = malloc(sizeof(grid_t));
-  if (grid == NULL) {
+  grid_t *grid = malloc(sizeof(grid_t));
+  if (grid == NULL)
+  {
     return NULL;
   }
 
   grid->size = size;
-  grid->cells = malloc(size * sizeof(colors_t*));
-  if (grid->cells && NULL) {
+  grid->cells = malloc(size * sizeof(colors_t *));
+  if (grid->cells && NULL)
+  {
     return NULL;
   }
 
-  for (size_t i = 0; i < size; ++i) {
+  for (size_t i = 0; i < size; ++i)
+  {
     grid->cells[i] = malloc(size * sizeof(colors_t));
-    if (grid->cells == NULL) {
+    if (grid->cells == NULL)
+    {
       return NULL;
     }
   }
@@ -74,13 +80,15 @@ grid_alloc(size_t size) {
   return grid;
 }
 
-void
-grid_free(grid_t* grid) {
-  if (!grid) {
+void grid_free(grid_t *grid)
+{
+  if (!grid)
+  {
     return;
   }
 
-  for (size_t i = 0; i < grid->size; ++i) {
+  for (size_t i = 0; i < grid->size; ++i)
+  {
     free(grid->cells[i]);
   }
 
@@ -88,19 +96,25 @@ grid_free(grid_t* grid) {
   free(grid);
 }
 
-void
-grid_print(const grid_t* grid, FILE* fd) {
-  if (!grid || !fd) {
+void grid_print(const grid_t *grid, FILE *fd)
+{
+  if (!grid || !fd)
+  {
     return;
   }
 
-  for (size_t i = 0; i < grid_get_size(grid); i++) {
-    for (size_t j = 0; j < grid_get_size(grid); j++) {
-      char* cell_content = grid_get_cell(grid, i, j);
-      if (cell_content) {
+  for (size_t i = 0; i < grid_get_size(grid); i++)
+  {
+    for (size_t j = 0; j < grid_get_size(grid); j++)
+    {
+      char *cell_content = grid_get_cell(grid, i, j);
+      if (cell_content)
+      {
         fprintf(fd, "%s ", cell_content);
         free(cell_content);
-      } else {
+      }
+      else
+      {
         fprintf(fd, "%c ", EMPTY_CELL);
       }
     }
@@ -108,38 +122,44 @@ grid_print(const grid_t* grid, FILE* fd) {
   }
 }
 
-bool
-grid_check_size(const size_t size) {
-  switch (size) {
-    case 1:
-    case 4:
-    case 9:
-    case 16:
-    case 25:
-    case 36:
-    case 49:
-    case 64:
-      return true;
+bool grid_check_size(const size_t size)
+{
+  switch (size)
+  {
+  case 1:
+  case 4:
+  case 9:
+  case 16:
+  case 25:
+  case 36:
+  case 49:
+  case 64:
+    return true;
 
-    default:
-      return false;
+  default:
+    return false;
   }
 }
 
-grid_t*
-grid_copy(const grid_t* grid) {
-  if (!grid) {
+grid_t *
+grid_copy(const grid_t *grid)
+{
+  if (!grid)
+  {
     return NULL;
   }
 
-  grid_t* new_grid = grid_alloc(grid->size);
+  grid_t *new_grid = grid_alloc(grid->size);
 
-  if (!new_grid) {
+  if (!new_grid)
+  {
     return NULL;
   }
 
-  for (size_t i = 0; i < grid->size; i++) {
-    for (size_t j = 0; j < grid->size; j++) {
+  for (size_t i = 0; i < grid->size; i++)
+  {
+    for (size_t j = 0; j < grid->size; j++)
+    {
       new_grid->cells[i][j] = grid->cells[i][j];
     }
   }
@@ -147,16 +167,19 @@ grid_copy(const grid_t* grid) {
   return new_grid;
 }
 
-char*
-grid_get_cell(const grid_t* grid, const size_t row, const size_t column) {
-  if (!grid || row >= grid->size || column >= grid->size) {
+char *
+grid_get_cell(const grid_t *grid, const size_t row, const size_t column)
+{
+  if (!grid || row >= grid->size || column >= grid->size)
+  {
     return NULL;
   }
 
   char cell_content = grid->cells[row][column];
 
-  char* cell_str = (char*)malloc(2 * sizeof(char));
-  if (!cell_str) {
+  char *cell_str = (char *)malloc(2 * sizeof(char));
+  if (!cell_str)
+  {
     return NULL;
   }
 
@@ -167,17 +190,20 @@ grid_get_cell(const grid_t* grid, const size_t row, const size_t column) {
 }
 
 size_t
-grid_get_size(const grid_t* grid) {
-  if (!grid) {
+grid_get_size(const grid_t *grid)
+{
+  if (!grid)
+  {
     return 0;
   }
   return grid->size;
 }
 
-void
-grid_set_cell(grid_t* grid, const size_t row, const size_t column,
-              const char color) {
-  if (!grid || row >= grid->size || column >= grid->size) {
+void grid_set_cell(grid_t *grid, const size_t row, const size_t column,
+                   const char color)
+{
+  if (!grid || row >= grid->size || column >= grid->size)
+  {
     return;
   }
 
