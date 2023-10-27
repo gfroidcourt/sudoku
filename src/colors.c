c5bdbad7b38d95b7256ab20ee70f5988739ef3de
Guillaume <guillaume.froidcourt@tricky.fr>
2023-10-27 19:03:06 +0200

    Oui

--- src/colors.c
+++ src/colors.c
@@ -5,13 +5,16 @@
 #include <stdio.h>
 
 colors_t
-colors_full(const size_t size) {
+colors_full(const size_t size)
+{
   static const int BITS_PER_BYTE = 8;
-  if (size == 0) {
+  if (size == 0)
+  {
     return 0ULL;
   }
 
-  if (size >= sizeof(colors_t) * BITS_PER_BYTE) {
+  if (size >= sizeof(colors_t) * BITS_PER_BYTE)
+  {
     return (colors_t)-1;
   }
 
@@ -19,13 +22,16 @@ colors_full(const size_t size) {
 }
 
 colors_t
-colors_empty(void) {
+colors_empty(void)
+{
   return 0ULL;
 }
 
 colors_t
-colors_set(const size_t color_id) {
-  if (color_id >= MAX_COLORS) {
+colors_set(const size_t color_id)
+{
+  if (color_id >= MAX_COLORS)
+  {
     return 0ULL;
   }
 
@@ -33,8 +39,10 @@ colors_set(const size_t color_id) {
 }
 
 colors_t
-colors_add(const colors_t colors, const size_t color_id) {
-  if (color_id >= MAX_COLORS) {
+colors_add(const colors_t colors, const size_t color_id)
+{
+  if (color_id >= MAX_COLORS)
+  {
     return colors;
   }
 
@@ -42,17 +50,20 @@ colors_add(const colors_t colors, const size_t color_id) {
 }
 
 colors_t
-colors_discard(const colors_t colors, const size_t color_id) {
-  if (color_id >= MAX_COLORS) {
+colors_discard(const colors_t colors, const size_t color_id)
+{
+  if (color_id >= MAX_COLORS)
+  {
     return colors;
   }
 
   return colors & ~(1ULL << color_id);
 }
 
-bool
-colors_is_in(const colors_t colors, const size_t color_id) {
-  if (color_id >= MAX_COLORS) {
+bool colors_is_in(const colors_t colors, const size_t color_id)
+{
+  if (color_id >= MAX_COLORS)
+  {
     return false;
   }
 
@@ -60,51 +71,58 @@ colors_is_in(const colors_t colors, const size_t color_id) {
 }
 
 colors_t
-colors_negate(const colors_t colors) {
+colors_negate(const colors_t colors)
+{
   return ~colors;
 }
 
 colors_t
-colors_and(const colors_t colors1, const colors_t colors2) {
+colors_and(const colors_t colors1, const colors_t colors2)
+{
   return colors1 & colors2;
 }
 
 colors_t
-colors_or(const colors_t colors1, const colors_t colors2) {
+colors_or(const colors_t colors1, const colors_t colors2)
+{
   return colors1 | colors2;
 }
 
 colors_t
-colors_xor(const colors_t colors1, const colors_t colors2) {
+colors_xor(const colors_t colors1, const colors_t colors2)
+{
   return colors1 ^ colors2;
 }
 
 colors_t
-colors_subtract(const colors_t colors1, const colors_t colors2) {
+colors_subtract(const colors_t colors1, const colors_t colors2)
+{
   return colors1 & ~colors2;
 }
 
-bool
-colors_is_equal(const colors_t colors1, const colors_t colors2) {
+bool colors_is_equal(const colors_t colors1, const colors_t colors2)
+{
   return colors1 == colors2;
 }
 
-bool
-colors_is_subset(const colors_t colors1, const colors_t colors2) {
+bool colors_is_subset(const colors_t colors1, const colors_t colors2)
+{
   return (colors1 & colors2) == colors1;
 }
 
-bool
-colors_is_singleton(const colors_t colors) {
+bool colors_is_singleton(const colors_t colors)
+{
   return colors && !(colors & (colors - 1));
 }
 
 size_t
-colors_count(const colors_t colors) {
+colors_count(const colors_t colors)
+{
   size_t count = 0;
   colors_t temp = colors;
 
-  while (temp) {
+  while (temp)
+  {
     temp = temp & (temp - 1);
     count++;
   }
@@ -113,19 +131,24 @@ colors_count(const colors_t colors) {
 }
 
 colors_t
-colors_rightmost(const colors_t colors) {
+colors_rightmost(const colors_t colors)
+{
   return colors & (-colors);
 }
 
 colors_t
-colors_leftmost(const colors_t colors) {
-  if (colors == 0) {
+colors_leftmost(const colors_t colors)
+{
+  if (colors == 0)
+  {
     return 0;
   }
 
   colors_t mask = 1ULL << (MAX_COLORS - 1);
-  for (size_t i = 0; i < MAX_COLORS; i++) {
-    if (colors & mask) {
+  for (size_t i = 0; i < MAX_COLORS; i++)
+  {
+    if (colors & mask)
+    {
       return mask;
     }
     mask >>= 1;
@@ -135,7 +158,8 @@ colors_leftmost(const colors_t colors) {
 }
 
 static colors_t
-prng(uint64_t* seed) {
+prng(uint64_t *seed)
+{
   uint64_t A = 6364136223846793005ULL;
   uint64_t C = 1ULL;
   *seed = *seed * A + C;
@@ -146,12 +170,14 @@ prng(uint64_t* seed) {
  * @brief Returns a random color from the provided set of colors.
  */
 colors_t
-colors_random(const colors_t colors) {
+colors_random(const colors_t colors)
+{
   static uint64_t seed;
 
   size_t count = colors_count(colors);
 
-  if (count == 0) {
+  if (count == 0)
+  {
     return 0;
   }
 
@@ -159,41 +185,16 @@ colors_random(const colors_t colors) {
   size_t random_index = randomNumber % count;
   size_t found_colors = 0;
 
-  for (size_t i = 0; i < MAX_COLORS; i++) {
-    if ((colors & (1ULL << i)) != 0) {
-      if (found_colors == random_index) {
+  for (size_t i = 0; i < MAX_COLORS; i++)
+  {
+    if ((colors & (1ULL << i)) != 0)
+    {
+      if (found_colors == random_index)
+      {
         return (1ULL << i);
       }
       found_colors++;
     }
   }
   return 0;
-}
-
-bool
-subgrid_consistency(colors_t subgrid[], const size_t size) {
-  colors_t all_colors = colors_full(size);
-  colors_t seen_colors = colors_empty();
-
-  // There is no empty cell
-  for (size_t i = 0; i < size; ++i) {
-    if (subgrid[i] == colors_empty()) {
-      return false;
-    }
-
-    // There is no two singletons with the same color
-    if (colors_is_singleton(subgrid[i])
-        && colors_is_in(seen_colors, colors_rightmost(subgrid[i]))) {
-      return false;
-    }
-
-    seen_colors = colors_or(seen_colors, subgrid[i]);
-  }
-
-  // Each color must appears at least once
-  if (!colors_is_subset(all_colors, seen_colors)) {
-    return false;
-  }
-
-  return true;
 }
--- src/sudoku.c
+++ src/sudoku.c
@@ -13,10 +13,11 @@
 #include <string.h>
 
 static bool verbose = false;
-static FILE* output;
+static FILE *output;
 
 static void
-print_help(char* executable_name) {
+print_help(char *executable_name)
+{
   printf("Usage:\t%s [a|-o FILE|-v|-V|-h] FILE...\n"
          "\tsudoku -g[SIZE] [-u|-o FILE|-v|-V|-h]\n"
          "Solve or generate Sudoku grids of size: 1, 4, 9, 16, 25, 36, 49, 64\n"
@@ -33,11 +34,13 @@ print_help(char* executable_name) {
   exit(EXIT_SUCCESS);
 }
 
-static grid_t*
-file_parser(char* filename) {
-  FILE* file = fopen(filename, "r");
+static grid_t *
+file_parser(char *filename)
+{
+  FILE *file = fopen(filename, "r");
 
-  if (!file) {
+  if (!file)
+  {
     fprintf(stderr, "Error opening file \"%s\".\n", filename);
     exit(EXIT_FAILURE);
   }
@@ -49,42 +52,50 @@ file_parser(char* filename) {
   size_t index = 0;
   char c;
 
-  grid_t* grid = NULL;
+  grid_t *grid = NULL;
   size_t expected_row_length = 0;
   size_t row_count = 0;
 
   bool end_of_line = false;
 
-  while ((c = fgetc(file)) != EOF) {
-    if (c == '#') {
+  while ((c = fgetc(file)) != EOF)
+  {
+    if (c == '#')
+    {
       while ((c = fgetc(file)) != EOF && c != '\n')
         ;
       continue;
-
-    } else if (c == '\n') {
-      if (index == 0) {
+    }
+    else if (c == '\n')
+    {
+      if (index == 0)
+      {
         continue;
       }
 
       end_of_line = true;
 
-      if (row_count == 0) {
+      if (row_count == 0)
+      {
 
-        if (index == 0) {
+        if (index == 0)
+        {
           fprintf(stderr, "Error: Empty file.\n");
           fclose(file);
           exit(EXIT_FAILURE);
         }
 
         grid = grid_alloc(index);
-        if (!grid) {
+        if (!grid)
+        {
           fclose(file);
           exit(EXIT_FAILURE);
         }
 
         expected_row_length = index;
-
-      } else if (index != expected_row_length) {
+      }
+      else if (index != expected_row_length)
+      {
         fprintf(stderr, "Line %zu is malformed! (wrong number of columns)",
                 row_count + 1);
         grid_free(grid);
@@ -92,8 +103,10 @@ file_parser(char* filename) {
         exit(EXIT_FAILURE);
       }
 
-      for (size_t i = 0; i < index; i++) {
-        if (!grid_check_char(grid, row[i])) {
+      for (size_t i = 0; i < index; i++)
+      {
+        if (!grid_check_char(grid, row[i]))
+        {
           fprintf(stderr, "Error: Invalid character '%c' on line %zu.\n",
                   row[i], row_count + 1);
           grid_free(grid);
@@ -105,9 +118,11 @@ file_parser(char* filename) {
 
       row_count++;
       index = 0;
-
-    } else if (c != ' ' && c != '\t') {
-      if (index >= MAX_GRID_SIZE) {
+    }
+    else if (c != ' ' && c != '\t')
+    {
+      if (index >= MAX_GRID_SIZE)
+      {
         fprintf(stderr, "Error: Row on line %zu has too many columns.\n",
                 row_count + 1);
         fclose(file);
@@ -117,7 +132,8 @@ file_parser(char* filename) {
     }
   }
 
-  if (!grid) {
+  if (!grid)
+  {
     fprintf(stderr, "Error: No valid grid found in the file.\n");
     exit(EXIT_FAILURE);
   }
@@ -125,12 +141,16 @@ file_parser(char* filename) {
   // Check for missing or extra lines
   size_t grid_size = grid_get_size(grid);
 
-  if (grid && row_count != grid_size) {
+  if (grid && row_count != grid_size)
+  {
 
-    if (row_count < grid_size) {
+    if (row_count < grid_size)
+    {
       fprintf(stderr, "Error: grid has %zu missing lines(s)  \n",
               grid_size - row_count);
-    } else {
+    }
+    else
+    {
       fprintf(stderr, "Error: grid has %zu extra line(s)  \n",
               row_count - grid_size);
     }
@@ -142,14 +162,14 @@ file_parser(char* filename) {
   return grid;
 }
 
-int
-main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   int optc;
   bool all = false;
   bool unique = false;
   bool generate = false;
   int result;
-  char* filename = NULL;
+  char *filename = NULL;
 
   const struct option options[] = {{"help", no_argument, NULL, 'h'},
                                    {"all", no_argument, NULL, 'a'},
@@ -162,104 +182,107 @@ main(int argc, char* argv[]) {
 
   output = stdout;
 
-  char* program_name = basename(argv[0]);
+  char *program_name = basename(argv[0]);
 
-  while ((optc = getopt_long(argc, argv, "havg::uo:V", options, NULL)) != -1) {
-    switch (optc) {
-      case 'h':
-        print_help(program_name);
-        break;
+  while ((optc = getopt_long(argc, argv, "havg::uo:V", options, NULL)) != -1)
+  {
+    switch (optc)
+    {
+    case 'h':
+      print_help(program_name);
+      break;
 
-      case 'V':
-        printf("%s version %d.%d.%d\n"
-               "Solve/generate sudoku grids of size: 1, 4, 9, 16, 25, "
-               "36, 49, 64\n",
-               program_name, VERSION, SUBVERSION, REVISION);
+    case 'V':
+      printf("%s version %d.%d.%d\n"
+             "Solve/generate sudoku grids of size: 1, 4, 9, 16, 25, "
+             "36, 49, 64\n",
+             program_name, VERSION, SUBVERSION, REVISION);
+      exit(EXIT_SUCCESS);
+
+    case 'v':
+      verbose = true;
+      break;
+
+    case 'o':
+      // output = fopen(optarg, "w");
+      filename = optarg;
+
+      if (output == NULL)
+      {
+        err(EXIT_FAILURE, "Error opening file: %s", optarg);
+      }
+      break;
+
+    case 'a':
+      if (generate)
+      {
+        warnx("warning: option 'all' conflict with generator mode, "
+              "disabling it !");
+      }
+      all = true;
+      printf("search for all possible solutions\n");
+      break;
+
+    case 'u':
+      if (!generate)
+      {
+        warnx("warning: option 'unique' conflict with solver mode, "
+              "disabling it !");
+      }
+      unique = true;
+      printf("generate a grid with unique solution\n");
+      break;
+
+    case 'g':
+      generate = true;
+
+      if (optarg == NULL)
+      {
+        printf("generate grid of size 9x9\n");
         exit(EXIT_SUCCESS);
+      }
 
-      case 'v':
-        verbose = true;
-        break;
+      if (!grid_check_size(atoi(optarg)))
+      {
+        errx(EXIT_FAILURE, "error: invalid grid size: %s", optarg);
+      }
 
-      case 'o':
-        // output = fopen(optarg, "w");
-        filename = optarg;
+      printf("generate grid of size %dx%d\n", atoi(optarg), atoi(optarg));
+      exit(EXIT_SUCCESS);
+      break;
 
-        if (output == NULL) {
-          err(EXIT_FAILURE, "Error opening file: %s", optarg);
-        }
-        break;
-
-      case 'a':
-        if (generate) {
-          warnx("warning: option 'all' conflict with generator mode, "
-                "disabling it !");
-        }
-        all = true;
-        printf("search for all possible solutions\n");
-        break;
-
-      case 'u':
-        if (!generate) {
-          warnx("warning: option 'unique' conflict with solver mode, "
-                "disabling it !");
-        }
-        unique = true;
-        printf("generate a grid with unique solution\n");
-        break;
-
-      case 'g':
-        generate = true;
-
-        if (optarg == NULL) {
-          printf("generate grid of size 9x9\n");
-          exit(EXIT_SUCCESS);
-        }
-
-        if (!grid_check_size(atoi(optarg))) {
-          errx(EXIT_FAILURE, "error: invalid grid size: %s", optarg);
-        }
-
-        printf("generate grid of size %dx%d\n", atoi(optarg), atoi(optarg));
-        exit(EXIT_SUCCESS);
-        break;
-
-      default:
-        errx(EXIT_FAILURE, "error: invalid option: %s", optarg);
+    default:
+      errx(EXIT_FAILURE, "error: invalid option: %s", optarg);
     }
   }
 
-  if (optind >= argc) {
+  if (optind >= argc)
+  {
     fprintf(stderr, "Error: no input file specified.\n");
     fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  if (filename != NULL) {
+  if (filename != NULL)
+  {
     output = fopen(filename, "w");
-    if (output == NULL) {
+    if (output == NULL)
+    {
       err(EXIT_FAILURE, "Error opening file: %s", filename);
     }
   }
 
-  for (int i = optind; i < argc; ++i) {
-    grid_t* grid = file_parser(argv[i]);
+  for (int i = optind; i < argc; ++i)
+  {
+    grid_t *grid = file_parser(argv[i]);
 
     grid_print(grid, output);
-
-    if (!grid_is_consistent(grid)) {
-      printf("The grid is inconsistent!\n");
-      grid_free(grid);
-      return EXIT_FAILURE;
-
-    } else {
-      printf("The grid is consistent.\n");
-    }
 
     grid_free(grid);
   }
 
-  if (output != stdout) {
+  if (output != stdout)
+  {
     fclose(output);
   }
 
